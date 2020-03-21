925706ca46c1f00d13a318c461f6d751247eea9f
zhangpeng <z53137473@126.com>
2020-03-21 14:20:12 +0800

    增加配置：springboot 不拦截静态资源

---
+++ src/main/java/com/pengcheng/template/MyWebMvcConfigurerAdapter.java
@@ -0,0 +1,15 @@
+package com.pengcheng.template;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+}
